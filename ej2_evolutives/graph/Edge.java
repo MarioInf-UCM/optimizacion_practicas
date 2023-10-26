3bd683e12a43b506921d300de9e01637e698f04c
marioa25 <145346934+marioa25@users.noreply.github.com>
2023-10-26 16:12:35 +0200

    Algoritmos evolutivos (#1)
    * Agregado contenido desarrollado previamente sobre grafos y el algoritmo MatchingMaximal
    
    * Desarrollo del ejecicio evolutivos

---
+++ ej2_evolutives/graph/Edge.java
@@ -0,0 +1,60 @@
+package graph;
+
+public class Edge{
+
+    private int id = -1; 
+    private int value = -1;
+    private int idSource = -1;
+    private int idDestine = -1;
+
+    public Edge(){
+    }
+    public Edge(Edge nEdge){
+        setId(nEdge.getId());
+        setValue(nEdge.getValue());
+        setIdSource(nEdge.getIdSource());
+        setIdDestine(nEdge.getIdDestine());
+    }
+    public Edge(int nId, int nValue, int nIdSource, int nIdDestine){
+        setId(nId);
+        setValue(nValue);
+        setIdSource(nIdSource);
+        setIdDestine(nIdDestine);
+    }   
+    public Edge(int nIdSource, int nIdDestine){
+        setId(-1);
+        setValue(-1);
+        setIdSource(nIdSource);
+        setIdDestine(nIdDestine);
+    }   
+
+    
+    /*******************************
+     * ZONA DE DEFINICIÓN DE MÉTODOS 
+     *******************************/
+
+    public void printEdgeComplet(){
+        System.out.print(" <"+this.getId()+" | "+this.getValue()+" | " +this.getIdSource()+"-" + this.getIdDestine()+">");
+    }
+
+    public void printEdgeSimple(){
+        System.out.print(" <"+this.getIdSource()+"-"+this.getIdDestine()+">");
+    }
+
+
+    /***************************************************************************
+     * ZONA DE DEFINICIÓN DE MÉTODOS PARA EL TRATAMIENTO BÁSICO DE LOS ATRIBUTOS
+     ***************************************************************************/
+    public int getId(){ return this.id; }
+    public void setId(int n){ this.id=n; }
+
+    public int getValue(){ return this.value; }
+    public void setValue(int n){ this.value=n; }
+
+    public int getIdSource(){ return this.idSource; }
+    public void setIdSource(int n){ this.idSource=n; }
+
+    public int getIdDestine(){ return this.idDestine; }
+    public void setIdDestine(int n){ this.idDestine=n; }
+
+}
---
+++ ej2_evolutives/graph/Node.java
@@ -0,0 +1,83 @@
+package graph;
+
+import java.util.ArrayList;
+
+public class Node{
+
+    private int id = -1; 
+    private int value = -1;
+    private ArrayList<Edge> edgeList = new ArrayList<Edge>();
+
+
+    public Node(){
+    }
+    public Node(Node nNode){
+        this.setId(nNode.getId());
+        this.setValue(nNode.getValue());
+        this.setEdgeList_duplicate(nNode.getEdgeList());
+    }
+    public Node(int nId, int nValue, ArrayList<Edge> nEdgeList){
+        this.setId(nId);
+        this.setValue(nValue);
+        if(nEdgeList!=null){
+            this.edgeList = nEdgeList;
+        }
+    }
+
+
+    /*******************************
+     * ZONA DE DEFINICIÓN DE MÉTODOS 
+     *******************************/
+
+    public void addEdge(Edge n){
+        this.edgeList.add(n);
+    }
+
+    public void printNodeComplet(){
+        System.out.print("\t[ "+this.getId()+" | "+this.getValue()+" ]");
+        for(int i=0 ; i<this.edgeList.size() ; i++){
+            this.edgeList.get(i).printEdgeComplet();
+        }
+        System.out.println("");
+    }
+
+    public void printNodeSimple(){
+        System.out.print("\t[ "+this.getId()+" | "+this.getValue()+" ]");
+        for(int i=0 ; i<this.edgeList.size() ; i++){
+            this.edgeList.get(i).printEdgeSimple();
+        }
+        System.out.println("");
+    }
+
+
+
+    /*********************************************************************
+     * ZONA DE DEFINICIÓN DE MÉTODOS PARA EL TRATAMIENTO DE LOS ATRIBUTOS
+     *********************************************************************/
+    public int getId(){ return id; }
+    public void setId(int n){ this.id=n; }
+
+    public int getValue(){ return value; }
+    public void setValue(int n){ value=n; }
+
+    public ArrayList<Edge> getEdgeList(){ return edgeList; }
+    public void setEdgeList_reference(ArrayList<Edge> n){this.edgeList = new ArrayList<Edge>(n);}
+    public void setEdgeList_duplicate(ArrayList<Edge> n){ 
+        this.edgeList = new ArrayList<Edge>();
+        Edge edghTemp;
+        for(int i=0 ; i<n.size() ; i++){
+            edghTemp = new Edge(n.get(i));
+            this.edgeList.add(edghTemp);
+        }
+    }
+    public void modifyEdgeList_addEdge(Edge n){
+        Edge nEdge = new Edge(n);
+        this.edgeList.add(nEdge);
+    }
+    public void modifyEdgeList_deleteAllNodes(){
+        this.getEdgeList().clear();
+    }
+
+
+}
+
