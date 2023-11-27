1204dab283ab85a664060661f6d4c1775589ce69
marioa25 <marioa25@ucm.es>
2023-11-27 18:29:09 +0100

    finalizado algoritmo de optimización de hormigas

--- ej4_AntColonyOptimization/src/main/Main.java
+++ ej4_AntColonyOptimization/src/main/Main.java
@@ -18,6 +18,7 @@ public class Main{
     private static int iterations = 0;
     private static float pheromoneControl = 0.0f;
     private static float desirabilityControl = 0.0f;
+    private static float rateEvaporationPheromone = 0.0f;
 
 
     public static void main(String[] args) throws FileNotFoundException, IOException{
@@ -32,6 +33,7 @@ public class Main{
         Main.iterations = Integer.parseInt(args[1]);
         Main.pheromoneControl = Float.parseFloat(args[2]);
         Main.desirabilityControl = Float.parseFloat(args[3]);
+        Main.rateEvaporationPheromone = Float.parseFloat(args[4]);
 
         Scanner sc = new Scanner(System.in);
         Graph graph = Graph.ensamblePonderateGraph_byFile(graphFileURL);
@@ -40,92 +42,136 @@ public class Main{
         Edge selectedEdge = null;
         boolean finishAnt_Fail = false;
         boolean finishAnt_Success = false;
+        boolean flagEnd = false;
+        String input;
         int movementDoneCount = 0;
-
+        float pheromoneTemp = 0.0f;
         ArrayList<Edge>finalPath = new ArrayList<Edge>();
         float pathDistance = 0.0f, minPathDistance=Float.MAX_VALUE;
+
 
         if(VERBOSE_MODE){
             System.out.println("Grafo inicial..:");
             graph.printGraphComplet();
         }
-        for(int hormiga=0 ; hormiga<iterations ; hormiga++){
-            
-                resetGraphValue(graph);
-                actualNodePosition = 0;
-                movementDoneCount = 0;
-                finishAnt_Fail = false;
-                finishAnt_Success = false;
-                actualNode = graph.getNode_byIndex(actualNodePosition);
-                finalPath = new ArrayList<Edge>();
-                pathDistance = 0.0f;
-            
-            do {    // Calculamos el recorido de una hormiga
 
-                // Paso 1: Verificamos si existe un camino disponible
-                graph.getNode_byIndex(actualNodePosition).setValue(0);
-                finishAnt_Fail = false;
-                finishAnt_Success = false;
-                if(!checkPosiblilities(graph, actualNode)){
-                    if(checkCompleteGraph(graph)){
-                        finishAnt_Success = true;
+        do{
+            for(int hormiga=0 ; hormiga<iterations ; hormiga++){
+                
+                    resetGraphValue(graph);
+                    actualNodePosition = 0;
+                    movementDoneCount = 0;
+                    finishAnt_Fail = false;
+                    finishAnt_Success = false;
+                    actualNode = graph.getNode_byIndex(actualNodePosition);
+                    finalPath = new ArrayList<Edge>();
+                    pathDistance = 0.0f;
+                
+                do {    // Calculamos el recorido de una hormiga
+
+                    // Paso 1: Verificamos si existe un camino disponible
+                    graph.getNode_byIndex(actualNodePosition).setValue(0);
+                    finishAnt_Fail = false;
+                    finishAnt_Success = false;
+                    if(!checkPosiblilities(graph, actualNode)){
+                        if(checkCompleteGraph(graph)){
+                            finishAnt_Success = true;
+                        }else{
+                            finishAnt_Fail = true;
+                        }
+                        break;
+                    }
+
+
+                    //Paso 2: Calculamos el proximo camino a recorrer
+                    calculateEdgesProbability(graph, actualNode);
+
+
+                    //Paso 3: Movemos a la hormiga
+                    selectedEdge = selectNextMovement(actualNode);
+                    finalPath.add(selectedEdge);
+
+                    if(VERBOSE_MODE){
+                        System.out.println("\n\nNº Hormiga: "+(hormiga+1)+"    nº movimiento "+(movementDoneCount+1));
+                        System.out.println("posición actual: "+(actualNodePosition+1)+ "    posición siguiente: "+selectedEdge.getIdDestine());
+                        graph.printGraphComplet();
+                    }
+
+                    graph.getNode_byIndex(actualNodePosition).setValue(1);
+                    actualNode = graph.getNode_byIndex(selectedEdge.getIdDestine()-1);
+                    actualNodePosition = selectedEdge.getIdDestine()-1;
+                    movementDoneCount++;
+
+                } while (!finishAnt_Fail && !finishAnt_Success); // Fin bucle de recorrido de hormigas
+                
+
+                //Paso 4: Actualización de feromonas
+                if(finishAnt_Success){
+                    pheromoneTemp = 0.0f;
+                    for(int i=0 ; i<finalPath.size() ; i++){
+                        pheromoneTemp = (1-rateEvaporationPheromone)*finalPath.get(i).getPheromone();
+                        pheromoneTemp = pheromoneTemp + (1/finalPath.get(i).getDistance());
+                        finalPath.get(i).setPheromone(pheromoneTemp);
+                    }
+                }
+
+
+                //Paso 5: Imprimir resultados
+                pathDistance=0; 
+                for(int i=0 ; i<finalPath.size() ; i++){
+                    pathDistance = pathDistance + finalPath.get(i).getDistance();
+                }
+
+                if(pathDistance < minPathDistance){
+                    minPathDistance = pathDistance;
+                    if(VERBOSE_MODE){
+                        System.out.println("\n\nÚltimo movimiento..:");
+                        System.out.println("Nº Hormiga: "+(hormiga+1)+"    nº movimiento "+(movementDoneCount+1));
+                        System.out.println("posición actual: "+(actualNodePosition+1) +"    Posición siguiente: De vuelta a casa");
+                        graph.printGraphComplet();
+                        
+                        if(finishAnt_Fail){
+                            System.out.println("La hormiga no ha encontrado el camino. La pobre ha muerto de inanición.");
+                        }else if(finishAnt_Success){
+                            System.out.println("¡¡La hormiga ha logrado encontrar el camino!!. Será recordada como un heroe.");
+                            printResult(finalPath, pathDistance);
+                            break;
+                        }       
                     }else{
-                        finishAnt_Fail = true;
+                        System.out.println("\nNuevo camino más optimo encontrado..:");
+                        System.out.println("Nº Hormiga: "+(hormiga+1)+"    nº movimiento "+(movementDoneCount+1));               
+                        printResult(finalPath, pathDistance);
+                    }
+                }
+                
+            } // Fin bucle de iteraciones (hormigas)
+                
+            System.out.println("\n\nFINALIZACIÓN..:");
+            System.out.println("Iteracion: " + iterations);
+            System.out.println("Quiere realizar más interacciones? (S/n)..: ");
+            do{
+                input = sc.nextLine().toUpperCase();
+                if (input.equals("S")) {
+                    while (true) {
+                        try {
+                            System.out.println("Introduzca el número de nuevas iteraciones a realizar..: ");
+                            iterations = iterations + sc.nextInt();
+                            break;
+                        } catch (Exception e) {
+                            System.out.println("ERROR..: " + e.getMessage());
+                        }
                     }
                     break;
+                } else if(input.equals("N")){
+                    flagEnd = true;
+                    break;
                 }
+            }while(true);
 
+        }while(!flagEnd);
 
-                //Paso 2: Calculamos el proximo camino a recorrer
-                calculateEdgesProbability(graph, actualNode);
-
-
-                //Paso 3: Movemos a la hormiga
-                selectedEdge = selectNextMovement(actualNode);
-                finalPath.add(selectedEdge);
-
-                if(VERBOSE_MODE){
-                    System.out.println("\n\nNº Hormiga: "+(hormiga+1)+"    nº movimiento "+(movementDoneCount+1));
-                    System.out.println("posición actual: "+(actualNodePosition+1)+ "    posición siguiente: "+selectedEdge.getIdDestine());
-                    graph.printGraphComplet();
-                }
-
-                graph.getNode_byIndex(actualNodePosition).setValue(1);
-                actualNode = graph.getNode_byIndex(selectedEdge.getIdDestine()-1);
-                actualNodePosition = selectedEdge.getIdDestine()-1;
-                movementDoneCount++;
-
-            } while (!finishAnt_Fail && !finishAnt_Success); // Fin bucle de recorrido de hormigas
-            
-            pathDistance=0; 
-            for(int i=0 ; i<finalPath.size() ; i++){
-                pathDistance = pathDistance + finalPath.get(i).getDistance();
-            }
-
-            if(pathDistance < minPathDistance){
-                minPathDistance = pathDistance;
-                if(VERBOSE_MODE){
-                    System.out.println("\n\nÚltimo movimiento..:");
-                    System.out.println("Nº Hormiga: "+(hormiga+1)+"    nº movimiento "+(movementDoneCount+1));
-                    System.out.println("posición actual: "+(actualNodePosition+1) +"    Posición siguiente: De vuelta a casa");
-                    graph.printGraphComplet();
-                    
-                    if(finishAnt_Fail){
-                        System.out.println("La hormiga no ha encontrado el camino. La pobre ha muerto de inanición.");
-                    }else if(finishAnt_Success){
-                        System.out.println("¡¡La hormiga ha logrado encontrar el camino!!. Será recordada como un heroe.");
-                        printResult(finalPath, pathDistance);
-                        break;
-                    }       
-                }else{
-                    System.out.println("\nNuevo camino más optimo encontrado..:");
-                    System.out.println("Nº Hormiga: "+(hormiga+1)+"    nº movimiento "+(movementDoneCount+1));               
-                    printResult(finalPath, pathDistance);
-                }
-            }
-            
-        } // Fin bucle de iteraciones (hormigas)
-
+        sc.close();
+        return;
     }
     
 
@@ -238,13 +284,14 @@ public class Main{
 
     private static Boolean checkInitialValues(String[] args) {
 
-        if (args.length != 4) {
+        if (args.length != 5) {
             System.out.println("Número de parámetros de entrada inválidos(" + args.length + "). La ejecución debe ser del tipo:");
-            System.out.println("java main \"grafoURL\" \"Iteraciones\" \"ControlFeromonas\" \"ControlDeseabilidad\"");
+            System.out.println("java main \"grafoURL\" \"Iteraciones\" \"ControlFeromonas\" \"ControlDeseabilidad\" \"tEvaporacionFeromonas\"");
             System.out.println("\tgrafoURL: Dirección dle archivo que contiene la definición del grafo.");
             System.out.println("\tIteraciones: Número de iteraciones a realizar (es decir, el número de hormigas).");
             System.out.println("\tControlFeromonas: Controla la influencia de las feromonas [0,1]");
             System.out.println("\tControlDeseabilidad: Controla la deseabilidad de los caminos [0,1]");
+            System.out.println("\tEvaporacionFeromonas: Controla el ratio de evaporación de las feromonas [0,1]");
             return false;
         }
         System.out.println("Ejecución algoritmo `Ant Colony Optimization'. Parámetros:");
@@ -252,6 +299,7 @@ public class Main{
         System.out.println("\tNúmero de iteraciones: " + args[1]);
         System.out.println("\tControl de feromonas: " + args[2]);
         System.out.println("\tControl de deseabilidad: " + args[3]);
+        System.out.println("\tRatio de evaporación: " + args[4]);
         return true;
     }
 
